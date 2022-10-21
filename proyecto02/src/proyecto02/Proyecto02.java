47556f054892e064bd7812af9749feb6733f1ff8
Gabriel Rojas <rgrojas4@utpl.edu.ec>
2022-10-21 12:57:38 -0500

    primer commit

--- proyecto02/src/proyecto02/Proyecto02.java
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -16,6 +16,10 @@ public class Proyecto02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ejemplo dos básico de Java");
+        System.out.println("Versionado a través de Git");
+        System.out.println("Manejado desde GitHub");
+        System.out.println("Además se usar GitHubClassroom");
     }
     
 }
